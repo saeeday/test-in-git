4bc00b9eb51a36b04900ae5d868670e246d5b9ed
saeed <leysi.saeed@gmail.com>
2019-12-19 17:45:08 +0330

    A constructor with two x,y parameter for A class and B class constructor updated.

--- src/com/aypara/example/A.java
+++ src/com/aypara/example/A.java
@@ -2,7 +2,8 @@ package com.aypara.example;
 
 public class A {
 	private int x,y;
-	public A() {
-		
+	public A(int x,int y) {
+		this.x=x;
+		this.y=y;
 	}
 }
--- src/com/aypara/example/B.java
+++ src/com/aypara/example/B.java
@@ -1,5 +1,10 @@
 package com.aypara.example;
 
 public class B extends A {
+
+	public B(int x, int y) {
+		super(x, y);
+		
+	}
 	
 }
